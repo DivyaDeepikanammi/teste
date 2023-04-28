02980584ef9128ec35672fa4dfad9d46bfff6143
DivyaDeepikanammi <127377310+DivyaDeepikanammi@users.noreply.github.com>
2023-04-28 14:27:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.write(' ')
 with col2:
-    image = Image.open("C:\\Users\\divya\\OneDrive\\Desktop\\Work\\Vintern\\App\\Logo.png")
+    image = Image.open("Logo.png")
     st.image(image)
 with col3:
     st.write(' ')
