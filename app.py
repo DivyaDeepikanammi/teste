b20630605e18bb54249c2e4955c67852cef96eb5
DivyaDeepikanammi <127377310+DivyaDeepikanammi@users.noreply.github.com>
2023-04-29 09:51:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from nltk.corpus import stopwords
 nltk.download('punkt')
 nltk.download('corpus')
 nltk.download('stopwords')
-from nltk.corpus.reader.plaintext import PlaintextParser
+#from nltk.corpus.reader.plaintext import PlaintextParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
